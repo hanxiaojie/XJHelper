74b3df2946e44e16ca18ae29089e30c5c6bdb964
韩晓杰 <80002771@sf-express.com>
2018-12-29 18:07:44 +0800

    修改spec文件错误

---
+++ XJHelper/XJHelper/XJHelperManager.h
@@ -0,0 +1,17 @@
+//
+//  XJHelperManager.h
+//  XJHelper
+//
+//  Created by EvanTan on 2018/12/29.
+//  Copyright © 2018 顺丰科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XJHelperManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XJHelper/XJHelper/XJHelperManager.m
@@ -0,0 +1,13 @@
+//
+//  XJHelperManager.m
+//  XJHelper
+//
+//  Created by EvanTan on 2018/12/29.
+//  Copyright © 2018 顺丰科技. All rights reserved.
+//
+
+#import "XJHelperManager.h"
+
+@implementation XJHelperManager
+
+@end
